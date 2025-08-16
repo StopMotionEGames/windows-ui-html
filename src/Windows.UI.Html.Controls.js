3a9db0737e2cbc08f230a2993b6d449bcea63484
Ravi Widnef <163608714+StopMotionEGames@users.noreply.github.com>
2025-08-16 11:58:45 -0300

    Refactor of Windows.UI.Html.Controls
    Refactoring the old /src/js/Windows.UI.Html.Controls (now at src/Windows.UI.Html.Controls.js) to register custom elements for UI controls, load required styles, and initialize features like RevealLight and WuhcNodeRegistry.

---
+++ src/Windows.UI.Html.Controls.js
@@ -0,0 +1,42 @@
+import Button from "./js/controls/Button.js";
+import Checkbox from "./js/controls/CheckBox.js";
+import ProgressBar from "./js/controls/ProgressBar.js";
+import ProgressRing from "./js/controls/ProgressRing.js";
+import Slider from "./js/controls/Slider.js";
+import TextBox from "./js/controls/TextBox.js";
+import { loadStyles } from "./js/core/StyleLoader.js";
+import RevealLight from "./js/features/RevealLight.js";
+import WuhcNodeRegistry from "./js/features/WuhcNodeRegistry.js";
+
+globalThis.debug = true; // if true, console logs are enabled
+
+(async () => {
+  const styles = [
+    "themes/Common",
+    "themes/Colors",
+    "themes/Symbols",
+    "themes/RevealLight",
+    "themes/ControlsVariables",
+    "controls/Button",
+    "controls/CalendarDatePicker",
+    "controls/CalendarView",
+    "controls/Checkbox",
+    "controls/PasswordBox",
+    "controls/ProgressBar",
+    "controls/ProgressRing",
+    "controls/Slider",
+    "controls/TextBox",
+  ];
+  loadStyles(styles);
+
+  customElements.define("wuhc-button", Button, { extends: "button" });
+  customElements.define("wuhc-checkbox", Checkbox);
+  customElements.define("wuhc-progressbar", ProgressBar);
+  customElements.define("wuhc-progressring", ProgressRing);
+  customElements.define("wuhc-slider", Slider);
+  customElements.define("wuhc-textbox", TextBox);
+
+  // Initialize features
+  WuhcNodeRegistry.init();
+  RevealLight.init()
+})();
