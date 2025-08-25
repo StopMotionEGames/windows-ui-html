ebe8770dae099ec1f029adc707a84b3e081a8518
Ravi Widnef <163608714+StopMotionEGames@users.noreply.github.com>
2025-08-25 16:11:14 -0300

    Update Utilities.js
    comments in english, now it's a class

--- src/js/core/Utilities.js
+++ src/js/core/Utilities.js
@@ -62,24 +62,58 @@ const RESERVED_WORDS = new Set([
   "length",
   "toString",
 ]);
-// Função para validar nomes de variáveis
-export const isValidVariableName = (name) => {
-  // Verifica caracteres inválidos
-  if (!/^[a-zA-Z_$][\w$]*$/.test(name)) return false;
+export default class Utilities {
+  /** Validade a variable name from a string.
+   *
+   * ---
+   * @returns {boolean} true if the name is valid
+   */
+  static IsValidVariableName(name) {
+    // Check for invalid characters
+    if (!/^[a-zA-Z_$][\w$]*$/.test(name)) return false;
 
-  // Verifica palavras reservadas
-  if (RESERVED_WORDS.has(name)) return false;
+    // Check for reserved words
+    if (RESERVED_WORDS.has(name)) return false;
 
-  // Verifica propriedades perigosas do prototype
-  if (name in Object.prototype || name in Function.prototype) return false;
+    // Check dengerous properties in prototype
+    if (name in Object.prototype || name in Function.prototype) return false;
 
-  return true;
-};
-
-export function getRelativeCoordinates(event, element) {
-  const rect = element.getBoundingClientRect();
-  return {
-    x: event.clientX - rect.left,
-    y: event.clientY - rect.top,
-  };
+    return true;
+  }
+  /** Get the relative coordenates of an element referent to pointer position
+   *
+   * ---
+   * @param {MouseEvent}event
+   * @param {HTMLElement}element
+   * @returns {{x:number,y:number}}
+   */
+  static GetRelativeCoordinates(event, element) {
+    const rect = element.getBoundingClientRect();
+    return {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    };
+  }
+  /** Converts a string to Pascal Case.
+   *
+   * ---
+   * There are 3 available separtors:
+   *
+   * * "-" dashes
+   * * "_" underlines
+   * * " " white spaces
+   *
+   * @param {string} str
+   * @returns {string}
+   */
+  static ToPascalCase(str) {
+    return str
+      .toLowerCase() // Convert entire string to lower case
+      .replace(/[-_\s]+(.)?/g, (_, char) => (char ? char.toUpperCase() : "")) // Captalize the first letter after separator and remove it
+      .replace(/^\w/, (char) => char.toUpperCase()); // Captalize the first letter of string
+  }
+  static CalculateProgress(value, min, max) {
+    if (max - min === 0) return 0;
+    return ((value - min) / (max - min)) * 100;
+  }
 }
