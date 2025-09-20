51cdbaa0d74a81acd24a25a9e17e2782954c7777
Ravi Widnef <163608714+StopMotionEGames@users.noreply.github.com>
2025-09-20 00:26:22 -0300

    Update Slider.js
    Modified UpdateValue function

--- src/js/controls/Slider.js
+++ src/js/controls/Slider.js
@@ -6,11 +6,18 @@ import WuhcValueInputControl from "./WuhcValueInputControl.js";
  * Web version of Slider from Windows 10
  */
 export default class Slider extends WuhcValueInputControl {
+  UpdateValue() {
+    this.inputElement.value = `${this._properties.Value}`;
+    this.UpdateProgress();
+  }
   InitializeControl() {
     this.appendChild(this.inputElement);
     this.inputElement.value = this._value;
     this.inputElement.type = "range";
-    this.inputElement.addEventListener("input", () => this.Value = Number(this.inputElement.value));
+    this.inputElement.addEventListener(
+      "input",
+      () => (this.Value = Number(this.inputElement.value))
+    );
     if (controlLogs)
       console.log(`${this.InitializeControl.name}: Initialized for`, this);
   }
